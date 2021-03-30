6bca036c2592a4b19089dc5dbbcc70f5c2eca121
nickon2805 <nikita_bondarenko01@mail.ru>
2021-03-30 21:49:02 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,32 @@
 $(function(){
   
+  $(".filter-products__star").rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+
+  $(".shop-item__star").rateYo({
+    starWidth: "17px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+  });
+
+$(".filter-price__input").ionRangeSlider({
+  type: "double",
+  prefix: "$",
+  onStart: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+  onChange: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+});
+
 $('.top-slider__inner').slick({
   dots: true,
   arrows:false,
